59a9a7286b3a3039dbf1bd5dd29d1173d1ddbadf
Peiiyong <jemins2001@gmail.com>
2025-04-03 13:09:05 +0800

    call api

--- t2_myfaker/lib/myfaker_screen.dart
+++ t2_myfaker/lib/myfaker_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MyFakerScreen extends StatefulWidget {
   const MyFakerScreen({super.key});
@@ -20,17 +22,26 @@ class _MyFakerScreenState extends State<MyFakerScreen> {
       body: Center(
         child: Column(
           children: [
-            ElevatedButton(
-              onPressed: onPressed, 
-              child: Text('Load Data'),
-            ),
+            ElevatedButton(onPressed: onPressed, child: Text('Load Data')),
           ],
-        )
+        ),
       ),
     );
   }
 
-  void onPressed() {
-    
+  Future<void> onPressed() async {
+    // Call the API and get the response
+    var response = await http.get(
+      Uri.parse('https://slumberjer.com/myfaker/myfaker.php'),
+    );
+    log(response.statusCode.toString());
+
+    log(response.body.toString());
+    /*
+    O U T P U T 
+    [log] 200
+    [log] {"status":"success","data":[{"name":"Dr. Liam Hartmann","phone":"886-508-4472 x77854",
+            "email":"lferry@yahoo.com","country":"Malta","address":"5842 Hassan Pass\nKellenborough, VA 29059"}]}
+    */
   }
 }
