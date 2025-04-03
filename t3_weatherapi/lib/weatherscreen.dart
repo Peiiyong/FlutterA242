46bac86b544fb50ffcee515ba09e0545004340ab
Peiiyong <jemins2001@gmail.com>
2025-04-03 14:24:16 +0800

    weather api is not accurate

--- t3_weatherapi/lib/weatherscreen.dart
+++ t3_weatherapi/lib/weatherscreen.dart
@@ -15,7 +15,6 @@ class _WeatherScreenState extends State<WeatherScreen> {
   final TextEditingController _controller = TextEditingController();
   List<dynamic> weatherData = [];
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,11 +30,28 @@ class _WeatherScreenState extends State<WeatherScreen> {
             TextField(controller: _controller),
             ElevatedButton(child: Text('Search'), onPressed: getWeather),
             Expanded(
-              child: weatherData.isEmpty? Text('no data'): ListView.builder(
-                itemCount: weatherData.length,
-                itemBuilder: (context, value) => 
-              ),
-            )
+              child:
+                  weatherData.isEmpty
+                      ? Text('no data')
+                      : ListView.builder(
+                        itemCount: weatherData.length,
+                        itemBuilder: (context, value) {
+                          String name = weatherData[value]['location']['location_name'];
+                          String date = weatherData[value]['date'];
+                          String morning = weatherData[value]['morning_forecast'];
+                          String afternoon = weatherData[value]['afternoon_forecast'];
+                          String evening = weatherData[value]['night_forecast'];
+                          String summary = weatherData[value]['summary_forecast'];
+
+                          return Card(
+                            child: ListTile(
+                              title: Text(name),
+                              subtitle: Text('$date\n$morning\n$afternoon\n$evening\n$summary'),
+                            ),
+                          );
+                        },
+                      ),
+            ),
           ],
         ),
       ),
@@ -45,19 +61,16 @@ class _WeatherScreenState extends State<WeatherScreen> {
   Future<void> getWeather() async {
     String loc = _controller.text;
     var response = await http.get(
-      Uri.parse(
-        'https://api.data.gov.my/weather/forecast?state=$loc',
-      ),
-    );
+      Uri.parse('https://api.data.gov.my/weather/forecast?state=$loc'), //unstable
+    ); //https://api.data.gov.my/weather/forecast/?contains=$loc@location__location_name *cant use this 
     log(response.body);
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       final List<dynamic> data = jsonDecode(response.body);
       setState(() {
         weatherData = data;
-      });  
+      });
     } else {
       log('Error: ${response.statusCode}');
     }
-
   }
 }
